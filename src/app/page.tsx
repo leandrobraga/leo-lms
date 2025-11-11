8c8d4ab80b433d1a27a637627521ff2cb60226bd
Leandro Quadros <leandro.quadros@gmail.com>
2025-11-11 15:35:28 -0300

    Login with github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function Home() {
+  const { data: session } = authClient.useSession();
+  const router = useRouter();
+
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/");
+          toast.success("Signed out Successfully");
+        },
+      },
+    });
+  }
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="p-24">
+      <h1 className="text-red-600 text-2xl font-bold">Hello World!</h1>
       <ThemeToggle />
+      {session ? (
+        <div>
+          <p>{session.user.name}</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+import { env } from "./env";
+import { emailOTP } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_SECRET,
+    },
+  },
+  plugins: [
+    emailOTP({
+      async sendVerificationOTP({ email, otp }) {
+        // implement sending te email to the user
+      },
+    }),
+  ],
+});
